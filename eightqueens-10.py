08de30ce25a440a1c0f254be596eda36023bf736
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2019-03-21 21:45:26 -0400

    Solution #10: no more gaps!

--- eightqueens-10.py
+++ eightqueens-10.py
@@ -15,35 +15,35 @@ x = """
     {
       "minInvariant": 0,
       "color": "yellow",
-      "maxInvariant": 0.094
+      "maxInvariant": 0.093
     },
     {
-      "minInvariant": 0.094,
+      "minInvariant": 0.093,
       "color": "green",
-      "maxInvariant": 0.222
+      "maxInvariant": 0.218
     },
     {
-      "minInvariant": 0.222,
+      "minInvariant": 0.218,
       "color": "orange",
-      "maxInvariant": 0.387
+      "maxInvariant": 0.391
     },
     {
-      "minInvariant": 0.387,
+      "minInvariant": 0.391,
       "color": "red",
-      "maxInvariant": 0.481
+      "maxInvariant": 0.488
     },
     {
-      "minInvariant": 0.481,
+      "minInvariant": 0.488,
       "color": "cyan",
-      "maxInvariant": 0.591
+      "maxInvariant": 0.605
     },
     {
-      "minInvariant": 0.756,
+      "minInvariant": 0.751,
       "color": "magenta",
-      "maxInvariant": 0.884
+      "maxInvariant": 0.885
     },
     {
-      "minInvariant": 0.884,
+      "minInvariant": 0.885,
       "color": "blue",
       "maxInvariant": 1
     }
@@ -52,72 +52,72 @@ x = """
     {
       "minInvariant": 0,
       "color": "green",
-      "maxInvariant": 0.454
+      "maxInvariant": 0.436
     },
     {
-      "minInvariant": 0.454,
+      "minInvariant": 0.436,
       "color": "red",
-      "maxInvariant": 0.674
+      "maxInvariant": 0.652
     },
     {
-      "minInvariant": 0.674,
+      "minInvariant": 0.652,
       "color": "yellow",
       "maxInvariant": 0.893
     },
     {
       "minInvariant": 0.893,
       "color": "cyan",
-      "maxInvariant": 1.087
+      "maxInvariant": 1.082
     },
     {
-      "minInvariant": 1.087,
+      "minInvariant": 1.082,
       "color": "orange",
-      "maxInvariant": 1.236
+      "maxInvariant": 1.228
     },
     {
-      "minInvariant": 1.236,
+      "minInvariant": 1.228,
       "color": "blue",
-      "maxInvariant": 1.367
+      "maxInvariant": 1.361
     },
     {
-      "minInvariant": 1.367,
+      "minInvariant": 1.361,
       "color": "magenta",
-      "maxInvariant": 1.559
+      "maxInvariant": 1.556
     }
   ],
   "b": [
     {
       "minInvariant": -1,
       "color": "yellow",
-      "maxInvariant": -0.627
+      "maxInvariant": -0.622
     },
     {
-      "minInvariant": -0.627,
+      "minInvariant": -0.622,
       "color": "orange",
-      "maxInvariant": -0.408
+      "maxInvariant": -0.407
     },
     {
-      "minInvariant": -0.183,
+      "minInvariant": -0.185,
       "color": "green",
-      "maxInvariant": -0.018
+      "maxInvariant": -0.004
     },
     {
-      "minInvariant": -0.018,
+      "minInvariant": -0.004,
       "color": "cyan",
-      "maxInvariant": 0.133
+      "maxInvariant": 0.150
     },
     {
-      "minInvariant": 0.133,
+      "minInvariant": 0.150,
       "color": "magenta",
-      "maxInvariant": 0.276
+      "maxInvariant": 0.300
     },
     {
-      "minInvariant": 0.276,
+      "minInvariant": 0.300,
       "color": "red",
-      "maxInvariant": 0.478
+      "maxInvariant": 0.487
     },
     {
-      "minInvariant": 0.478,
+      "minInvariant": 0.487,
       "color": "blue",
       "maxInvariant": 1
     }
@@ -126,59 +126,41 @@ x = """
     {
       "minInvariant": 0,
       "color": "red",
-      "maxInvariant": 0.157
+      "maxInvariant": 0.145
     },
     {
-      "minInvariant": 0.157,
+      "minInvariant": 0.145,
       "color": "green",
-      "maxInvariant": 0.284
+      "maxInvariant": 0.274
     },
     {
-      "minInvariant": 0.284,
+      "minInvariant": 0.274,
       "color": "blue",
-      "maxInvariant": 0.411
+      "maxInvariant": 0.403
     },
     {
-      "minInvariant": 0.411,
+      "minInvariant": 0.403,
       "color": "cyan",
-      "maxInvariant": 0.549
+      "maxInvariant": 0.542
     },
     {
-      "minInvariant": 0.549,
+      "minInvariant": 0.542,
       "color": "magenta",
-      "maxInvariant": 0.679
+      "maxInvariant": 0.670
     },
     {
-      "minInvariant": 0.679,
+      "minInvariant": 0.670,
       "color": "yellow",
-      "maxInvariant": 0.811
+      "maxInvariant": 0.805
     },
     {
-      "minInvariant": 0.811,
+      "minInvariant": 0.805,
       "color": "orange",
-      "maxInvariant": 0.916
+      "maxInvariant": 0.915
     }
   ]
 }
 """
-
-def shift_right(j, factor):
-    def inc(mn, mx):
-        def f(elt):
-            if elt["minInvariant"] != mn: elt["minInvariant"] += factor
-            if elt["maxInvariant"] != mx: elt["maxInvariant"] += factor
-            return elt
-        return f
-    def dec(mn, mx):
-        def f(elt):
-            if elt["minInvariant"] != mn: elt["minInvariant"] -= factor
-            if elt["maxInvariant"] != mx: elt["maxInvariant"] -= factor
-            return elt
-        return f
-    j["v"] = map(inc(0,1), j["v"])
-    j["s"] = map(inc(0,2), j["s"])
-    j["b"] = map(dec(-1,1), j["b"])
-    return j
 
 import json
 import urllib
--- eightqueens.md
+++ eightqueens.md
@@ -8,6 +8,6 @@
     Solution 07 (5cc): 0.0103
     Solution 08 (5cc): 0.0103
     Solution 09 (4cc): 0.0105 (but could be jiggled more)
-    Solution 10 (4cc): 0.0114 (1 gap) (CURRENT BEST)
+    Solution 10 (4cc): 0.0115 (0 gaps) (CURRENT BEST)
     Solution 11 (3cc): 0.0109 (1 gap)
     Solution 12 (6cc): 0.0076
